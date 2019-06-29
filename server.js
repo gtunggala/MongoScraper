93db56e7f20b7f1c66f75e24447dfe96328f9667
Gabriel Tunggala <gtunggala@gmail.com>
2019-06-28 20:40:06 -0400

    added public foler

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 const PORT = process.env.PORT || 3030;
+
 var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
@@ -19,6 +25,9 @@ mongoose.connect(
   }
 );
 
+app.get("/", function(req, res) {
+  res.sendFile(path.resolve(__dirname + "public/index.html"));
+});
 app.listen(PORT, function() {
   console.log("started on " + PORT);
 });
