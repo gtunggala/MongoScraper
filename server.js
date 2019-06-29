5243a3cd112709e44f33e240e58f976464c15f67
Gabriel Tunggala <gtunggala@gmail.com>
2019-06-28 20:23:24 -0400

    change port order

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
-
+const mongoose = require('mongoose');
 const app = express();
 
-const PORT = 3030 || process.env.PORT
+const PORT = process.env.PORT || 3030;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI)
 
 app.listen(PORT, function() {
     console.log('started on ' + PORT)
