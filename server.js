e95200b52a24d4de9a478a797b43c3f3bb73b578
Gabriel Tunggala <gtunggala@gmail.com>
2019-07-01 15:59:03 -0400

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const axios = require("axios");
 const app = express();
 
 // Parse request body as JSON
@@ -29,6 +31,44 @@ app.get("/", function(req, res) {
   res.sendFile(path.resolve(__dirname + "public/index.html"));
 });
 
+app.get("/scrape", function(req, res) {
+  let response;
+  const url = "https://www.npr.org/sections/news/";
+
+  axios
+    .get(url)
+    .then(function(response) {
+      //   we're loading the data from the response that's given to us by using cheerio
+      var $ = cheerio.load(response.data);
+
+      //   console.log(response.data);
+      $("article.item").each(function(i, art) {
+        let article = {};
+
+        article.title = $(this)
+          .find("h2.title")
+          .text();
+
+        article.teaser = $(this)
+          .find("p.teaser")
+          .text();
+
+        // trying to find Image, does not work
+        // article.image = $(this)
+        //   .find("img.respArchListImg")
+        //   .text();
+
+        console.log(article);
+      });
+
+      res.sendStatus(200);
+    })
+    .catch(function(error) {
+      console.log(error);
+      // do something
+      res.sendStatus(500);
+    });
+});
 app.listen(PORT, function() {
   console.log("started on " + PORT);
 });
